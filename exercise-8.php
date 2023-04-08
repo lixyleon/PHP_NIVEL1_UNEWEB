94a32bb1b85cd77a74b5307903230324289c08a7
lixyleon <lixyleon2000@gmail.com>
2023-04-08 09:45:50 -0400

    first commit

---
+++ exercise-8.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <title>salida y concatenacion de variables</title>
+</head>
+<body>
+    <h1>Variables en php</h1>
+<?php
+    $texto='programar';
+    
+    echo 'Yo amo $texto <br>'; //esto genera un error por el uso de la comilla simple para lectura de variable
+    // para tomar el valor intepretado de la variable se usará comillas dobles
+
+    echo "Yo amo $texto";
+ 
+    ?>
+   
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
+</body>
+</html>
