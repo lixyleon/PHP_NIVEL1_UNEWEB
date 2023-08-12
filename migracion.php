63ac6cb86309c5462433475dfff03333964fae6e
lixyleon <123250641+lixyleon@users.noreply.github.com>
2023-08-12 11:19:43 -0400

    Update migracion.php
    ahora si funciona

--- migracion.php
+++ migracion.php
@@ -1,47 +1,33 @@
 <?php
 
-function contarValoresArray($array)
+$result =[0,0,0,0,0];
 
-{
-
-	$contar=array();
-    $arr = array();
-    $i=0;
-
-	foreach($array as $value)
-
-	{
-
-		if(isset($contar[$value]))
-
-		{
-
-			// si ya existe, le añadimos uno
-
-			$contar[$value]+=1;
-           $arr[$i] = $value;
-           $i++;
-           
-
-		}else{
-
-			// si no existe lo añadimos al array
-
-			$contar[$value]=1;
-           
-		}
-
-	}
-
-	return $arr;
-
-
+for($i=0;$i<count($arr);$i++){
+    switch($arr[$i]){
+    case 1:
+        $result[0] += 1;
+        break;
+    case 2:
+        $result[1] += 1;
+        break;
+    case 3:
+        $result[2] += 1;
+        break;
+    case 4:
+        $result[3] += 1;
+        break;
+    case 5:
+        $result[4] += 1;
+        break;
+      
+    }
 }
-
- 
-
-$miArray=array(1, 5, 1, 5, 3);
-
+$max = max($result);
+for($i=0;$i<count($result);$i++){
+    if($result[$i]==$max){
+        return ++$i;
+    }
+}
 print_r(contarValoresArray($miArray));
 
 
